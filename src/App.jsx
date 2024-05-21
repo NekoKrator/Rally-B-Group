b524a1a8e2b62385316c8503fcfcdea10ddc6693
Fil Daniil <irarbc89@gmail.com>
2024-05-22 00:39:18 +0300

    First version of navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import './App.css'
+import Navbar from './components/Navbar/Navbar'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 function App() {
-  return (
-    <>
-    
-    </>
-  )
+    return (
+        <>
+            <Router>
+                <Navbar />
+                <Routes>
+                    <Route path="/" exact />
+                </Routes>
+            </Router>
+        </>
+    )
 }
 
 export default App
